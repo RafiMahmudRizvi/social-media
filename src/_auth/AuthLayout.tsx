0f7f040e449c40eadb17e362f8e8c73817703cac
encoderit-rafi <iamrafimahmudrizvi@gmail.com>
2024-02-15 23:20:25 +0600

    commit-2

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import { Outlet, Navigate } from "react-router-dom";
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 items-center justify-center">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover object-center bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
